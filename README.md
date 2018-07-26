144b2575a48287ec8c4088ef9cab311cdf73a4c3
freewind <nowindlee@gmail.com>
2018-07-26 16:58:51 +0800

    demo ok

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-Kotlin H2 Database Demo
-=======================
+Kotlin H2 Create Row by Form Demo
+=================================
 
-Create table and query it in H2.
+Dynamically get column names from ResultSet and build a form.
+When submitting the form, it will get all the values from the textfields and convert them to correct value(by the meta info get for the result set),
+then run the generated sql to create new row into h2 database.
 
-In `Hello.kt` file.
+
+Run `Hello.kt` in your IDE.
--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -1,42 +1,137 @@
 package example
 
+import com.github.freewind.lostlist.ArrayLists
+import javafx.beans.property.SimpleIntegerProperty
+import javafx.beans.property.SimpleStringProperty
+import javafx.collections.FXCollections.observableArrayList
+import javafx.scene.control.TableView
+import tornadofx.*
 import java.sql.DriverManager
+import java.sql.ResultSet
+import java.sql.ResultSetMetaData
+import java.sql.Types
 
-private val DBNAME = "mydb"
-
-fun main(args: Array<String>) {
+val connection = run {
     Class.forName("org.h2.Driver")
-
-    val conn = DriverManager.getConnection("jdbc:h2:mem:$DBNAME", "sa", "sa") // (2)
-
-    conn.use {
-
-        conn.createStatement().use { stmt ->
-            with(stmt) {
-                executeUpdate("create table mytbl(id int primary key, name varchar(255))")
-                executeUpdate("insert into mytbl values(1, 'Hello')")
-                executeUpdate("insert into mytbl values(2, 'World')")
-            }
-        }
-
-        conn.createStatement().use { stmt ->
-            val rs = stmt.executeQuery("select * from mytbl")
-            while (rs.next()) {
-                println("> " + rs.getString("name"))
-            }
-        }
-    }
-
+    DriverManager.getConnection("jdbc:h2:mem:mydb", "sa", "sa")!!
 }
 
-fun <T : AutoCloseable?, R> T.use(block: (T) -> R): R {
-    try {
-        return block(this)
-    } finally {
-        try {
-            this?.close()
-        } catch (e: Exception) {
-            println(e.toString())
+data class User(private val id: Int, private val name: String) {
+    val idProperty = SimpleIntegerProperty(id)
+    val nameProperty = SimpleStringProperty(name)
+}
+
+private val data = observableArrayList<User>()
+
+class HelloWorld : View() {
+
+    private val columnMetas = getColumnMetaOfUserTable()
+
+    private val user = mutableMapOf<String, SimpleStringProperty>().apply {
+        columnMetas.forEach { meta ->
+            this[meta.columnName] = SimpleStringProperty()
         }
     }
+
+    init {
+        data.addAll(loadDbUsers())
+    }
+
+    private lateinit var tableView: TableView<User>
+
+    override val root = hbox {
+        tableview<User>(data) {
+            tableView = this
+            column("id", User::idProperty)
+            column("name", User::nameProperty)
+        }
+        vbox {
+            form {
+                fieldset("New Row") {
+                    columnMetas.forEach { meta ->
+                        field(meta.columnName) {
+                            textfield(user[meta.columnName]!!)
+                        }
+                    }
+                }
+            }
+            button("Add to database").setOnAction {
+                createUser(user.mapValues { it.value.value })
+            }
+        }
+    }
+
+    private fun createUser(map: Map<String, String>) {
+        val fields = columnMetas.map { it.columnName }.joinToString(", ")
+
+        val sql = """insert into users($fields) values (${ArrayLists.createFilled(columnMetas.size, "?").joinToString(",")})"""
+        val stmt = connection.prepareStatement(sql)
+        columnMetas.forEachIndexed { i, meta ->
+            val index = i + 1
+            when (meta.type) {
+                Types.VARCHAR -> stmt.setString(index, map[meta.columnName])
+                Types.INTEGER -> stmt.setInt(index, map[meta.columnName]!!.toInt())
+                else -> println("Unhandled type: $meta")
+            }
+        }
+        stmt.execute()
+
+        data.clear()
+        data.addAll(loadDbUsers())
+    }
+
+    private fun getColumnMetaOfUserTable(): List<ColumnMeta> {
+        val rs = connection.createStatement().executeQuery("select * from users limit 0")
+        return getColumnMeta(rs)
+    }
+
+    private fun loadDbUsers(): List<User> {
+        val users = mutableListOf<User>()
+        val rs = connection.createStatement().executeQuery("select * from users")
+        while (rs.next()) {
+            users.add(User(id = rs.getInt("id"), name = rs.getString("name")))
+        }
+        return users
+    }
+}
+
+class HelloWorldStyle : Stylesheet() {
+    init {
+        root {
+            prefWidth = 600.px
+            prefHeight = 400.px
+        }
+    }
+}
+
+class HelloWorldApp : App(HelloWorld::class, HelloWorldStyle::class)
+
+// type: `java.sql.Types`
+data class ColumnMeta(val columnName: String, val displayName: String, val type: Int, val required: Boolean)
+
+private fun getColumnMeta(rs: ResultSet): List<ColumnMeta> {
+    val metaData = rs.metaData
+    return (1..metaData.columnCount).map { index ->
+        ColumnMeta(
+                columnName = metaData.getColumnName(index),
+                displayName = metaData.getColumnLabel(index),
+                type = metaData.getColumnType(index),
+                required = metaData.isNullable(index) == ResultSetMetaData.columnNoNulls
+        )
+    }.toList()
+}
+
+private fun prepareDbData() {
+    connection.createStatement().use { stmt ->
+        with(stmt) {
+            executeUpdate("create table users(id int primary key, name varchar(255))")
+            executeUpdate("insert into users values(1, 'Hello')")
+            executeUpdate("insert into users values(2, 'World')")
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    prepareDbData()
+    launch<HelloWorldApp>()
 }
